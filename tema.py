d22ce2caf3e18ace3f0fd31107b5bae8bda619e2
lorena.ana <lorena.ana@nttdata.ro>
2020-06-14 10:21:34 +0300

    "first init"

---
+++ tema.py
@@ -0,0 +1,15 @@
+
+#1. Reverse the order of the items in an array.
+a = [1, 2, 3, 4, 5]
+a.reverse()
+print(a)
+
+#2. Get the number of occurrences of var b in array a.
+x = [1, 1, 2, 2, 2, 2, 3, 3, 3]
+y = 2
+print("number of occurences for b", x.count(y))
+
+#3. Given a sentence as string, count the number of words in it.
+
+z="ana are mere si nu are pere"
+print(z.count("a"))
